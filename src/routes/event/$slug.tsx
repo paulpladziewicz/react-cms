b64a7596db64ed3d0a2864686645683a5ebec74e
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-11-06 10:09:56 -0500

    adjustment to event page layout

--- src/routes/event/$slug.tsx
+++ src/routes/event/$slug.tsx
@@ -1,58 +1,70 @@
-import { createFileRoute, useLocation } from '@tanstack/react-router'
+import {createFileRoute, Link, useLocation} from '@tanstack/react-router'
+import {Col, Container, Row} from "react-bootstrap";
 
 export const Route = createFileRoute('/event/$slug')({
-  component: RouteComponent,
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  const { state } = useLocation() // Access event data passed through state
-  const event = state?.event
+    const {state} = useLocation() // Access event data passed through state
+    const event = state?.event
 
-  if (!event) {
-    return <div>No event data available.</div>
-  }
+    if (!event) {
+        return <div>No event data available.</div>
+    }
 
-  const { detail, status } = event
-  const { title, description, locationName, address, days, formattedTimes } =
-    detail
+    const {detail, status} = event
+    const {title, description, locationName, address, days, formattedTimes} =
+        detail
 
-  return (
-    <div className="col-md-10 col-lg-8 mx-auto">
-      <section>
-        {status === 'CANCELED' && (
-          <p className="fw-semibold text-danger my-2">
-            This event has been canceled.
-          </p>
-        )}
+    return (
+        <Container>
+            <Row>
+                <Col md={8} lg={10} className="mx-auto">
+                    <nav aria-label="breadcrumb">
+                        <ol className="breadcrumb mb-2">
+                            <li className="breadcrumb-item"><Link to="/events">Events</Link></li>
+                            <li className="breadcrumb-item active" aria-current="page">{title}</li>
+                        </ol>
+                    </nav>
 
-        <div className="d-flex justify-content-between align-items-center mb-2">
-          <h1 className="display-4 mb-0">{title}</h1>
-        </div>
+                    <section>
+                        {status === 'CANCELED' && (
+                            <p className="fw-semibold text-danger my-2">
+                                This event has been canceled.
+                            </p>
+                        )}
 
-        <p
-          className="fs-xl mt-2 mb-4"
-          dangerouslySetInnerHTML={{ __html: description }}
-        />
+                        <div className="d-flex justify-content-between align-items-center mb-2">
+                            <h1 className="display-4 mb-0">{title}</h1>
+                        </div>
 
-        {locationName && <h3 className="h5 mb-0">{locationName}</h3>}
-        {address && <p>{address}</p>}
+                        <p
+                            className="fs-xl mt-2 mb-4"
+                            dangerouslySetInnerHTML={{__html: description}}
+                        />
 
-        {days && formattedTimes && (
-          <div className="mt-4">
-            {days.map((_, index) => (
-              <p key={index} className="fs-lg fw-bold mb-2">
-                {formattedTimes[index * 2]}
-                {formattedTimes[index * 2 + 1] !== 'No End Time' && (
-                  <>
-                    {' - '}
-                    {formattedTimes[index * 2 + 1]}
-                  </>
-                )}
-              </p>
-            ))}
-          </div>
-        )}
-      </section>
-    </div>
-  )
+                        {locationName && <h3 className="h5 mb-0">{locationName}</h3>}
+                        {address && <p>{address}</p>}
+
+                        {days && formattedTimes && (
+                            <div className="mt-4">
+                                {days.map((_, index) => (
+                                    <p key={index} className="fs-lg fw-bold mb-2">
+                                        {formattedTimes[index * 2]}
+                                        {formattedTimes[index * 2 + 1] !== 'No End Time' && (
+                                            <>
+                                                {' - '}
+                                                {formattedTimes[index * 2 + 1]}
+                                            </>
+                                        )}
+                                    </p>
+                                ))}
+                            </div>
+                        )}
+                    </section>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
