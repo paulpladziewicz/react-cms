f65a6a22dda69e2ea8491a8b873ad85b39d7bb25
Paul Pladziewicz <ppladziewicz@gmail.com>
2024-11-27 17:05:28 -0500

    starting coming soon page

--- src/routes/explore.tsx
+++ src/routes/explore.tsx
@@ -6,5 +6,20 @@ export const Route = createFileRoute('/explore')({
 })
 
 function RouteComponent() {
-  return 'Hello /explore!'
+  return (
+      <div>
+        <main className="page-wrapper py-5">
+          <div
+              className="container d-flex flex-column justify-content-center align-items-center h-100 text-center pb-2 py-sm-3 py-md-4 py-lg-5">
+
+
+            <h1 className="display-2 text-primary">Whipping up our website</h1>
+            <p className="fs-xl mx-auto mb-5" style={{maxWidth: '680px'}}>We can't wait to unveil what we've been
+              working
+              on, and we appreciate your patience. Thanks for being a part of our journey!</p>
+
+          </div>
+        </main>
+      </div>
+  )
 }
